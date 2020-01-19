14cb8785aea501d3fe33b94f055a3edbc224af9a
Yinon Vahab <yinonv@gmail.com>
2020-01-19 12:18:03 +0200

    image works

--- popup.js
+++ popup.js
@@ -1,4 +1,3 @@
-
 function handleOnLoad() {
     chrome.tabs.query({ currentWindow: true, active: true }, tabs => {
         chrome.tabs.sendMessage(tabs[0].id, 'give me url', getUrl)
@@ -9,13 +8,13 @@ async function getUrl(res) {
     const div = document.createElement('div')
     div.textContent = "url: " + res.url;
     document.body.append(div);
-    const response = await axios.get(`http://127.0.0.1:2700/data?url=${res.url}`)
-    const div2 = document.createElement('div')
-    div2.textContent = "image: " + response.data;
-    document.body.append(div2)
     img = document.createElement('img')
-    img.src = response.data;
+    img.classList.add('loading-img')
+    img.src = './loader.gif'
     document.body.append(img)
+    const response = await axios.get(`http://127.0.0.1:2700/data?url=${res.url}`)
+    img.classList.remove('loading-img')
+    img.src = response.data;
 }
 
 document.addEventListener('DOMContentLoaded', handleOnLoad)
