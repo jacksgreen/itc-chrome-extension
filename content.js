cf687a33d6e36057b1f28c302c125f5d30406723
Yinon Vahab <yinonv@gmail.com>
2020-01-19 13:17:27 +0200

    added background

--- content.js
+++ content.js
@@ -2,3 +2,5 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   sendResponse({url: window.location.href})
   // chrome.browserAction.setIcon({path:"./one.png"})
 })
+
+chrome.runtime.sendMessage({ "newIconPath" : "./one.png" });
