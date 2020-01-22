d876bfde163ad977f04c378c5137e9ec4bd9ef30
jackgreen <jack_s_green@me.com>
2020-01-22 21:35:03 +0200

    chrome extension details

--- popup.js
+++ popup.js
@@ -1,19 +1,35 @@
 function handleOnLoad() {
-    chrome.tabs.query({ currentWindow: true, active: true }, tabs => {
-        chrome.tabs.sendMessage(tabs[0].id, 'give me data', displayData)
-    })
+  chrome.tabs.query({ currentWindow: true, active: true }, tabs => {
+    chrome.tabs.sendMessage(tabs[0].id, 'give me data', displayData);
+  });
 }
 
 function displayData(res) {
+  box = document.getElementById('content-box');
 
+  brand = document.createElement('div');
+  brand.textContent = 'Brand: ' + res.data.mainProduct.brand;
+  box.append(brand);
+
+  composition = document.createElement('div');
+  composition.textContent = 'Composition: ' + res.data.mainProduct.composition;
+  box.append(composition);
+
+  type = document.createElement('div');
+  type.textContent = 'Type: ' + res.data.mainProduct.type;
+  box.append(type);
+
+  ecoscore = document.createElement('div');
+  ecoscore.textContent = 'Ecoscore: ' + res.data.mainProduct.ecoscore;
+  box.append(ecoscore);
 }
 
 document.addEventListener('DOMContentLoaded', handleOnLoad);
 const websiteBtn = document.getElementById('website-button');
-websiteBtn.addEventListener('click', handleWebsiteRedirect)
+websiteBtn.addEventListener('click', handleWebsiteRedirect);
 
 function handleWebsiteRedirect() {
-    //website = "https://wingspan-1.firebaseapp.com/"
-    website = "http://127.0.0.1:3000"
-    window.open(website)
+  //website = "https://wingspan-1.firebaseapp.com/"
+  website = 'http://127.0.0.1:3000';
+  window.open(website);
 }
