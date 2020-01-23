2a4709c779282e911f0d606a902ec0764489e8c8
Yinon Vahab <yinonv@gmail.com>
2020-01-23 13:44:53 +0200

    no change

--- background.js
+++ background.js
@@ -1,18 +1,17 @@
 chrome.runtime.onMessage.addListener((msg, sender, sendResponse) => {
     chrome.tabs.query({ active: true, windowType: "normal", currentWindow: true }, (d) => {
         let tabId = d[0].id;
-        if (msg.icon == "loader") {
-            let loader = 1
-            while (loader < 10) {
-                chrome.browserAction.setIcon({ path: `./numbers/${loader.toString()}.png`, tabId: tabId });
-                if (loader == 10) {
-                    loader = 1
-                } else {
-                    loader++
-                }
-            }
-        } else {
-            chrome.browserAction.setIcon({ path: `./numbers/${msg.icon}.png`, tabId: tabId });
-        }
+        // if (msg.icon == "loader") {
+        //     let loader = 1
+        //     while (loader < 10) {
+        //         chrome.browserAction.setIcon({ path: `./numbers/${loader.toString()}.png`, tabId: tabId });
+        //         if (loader == 10) {
+        //             loader = 1
+        //         } else {
+        //             loader++
+        //         }
+        //     }
+        // } 
+            chrome.browserAction.setIcon({ path: `./numbers/${msg.icon}.png`, tabId: tabId })
     })
 })
